830526064513e70775e18f3df8037b4ab3291998
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-04-12 12:11:08 +0200

    Add project files.

---
+++ Server/Areas/Identity/Data/LeanheatProject1ServerLogInContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeanheatProject1.Server.Data
+{
+    public class LeanheatProject1ServerLogInContext : IdentityDbContext<IdentityUser>
+    {
+        public LeanheatProject1ServerLogInContext(DbContextOptions<LeanheatProject1ServerLogInContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LeanheatProject1.Server.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LeanheatProject1.Server.Areas.Identity.IdentityHostingStartup))]
+namespace LeanheatProject1.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<LeanheatProject1ServerLogInContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("LeanheatProject1ServerContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<LeanheatProject1ServerLogInContext>();
+            });
+        }
+    }
+}
